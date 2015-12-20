acf920b335abbcfcef39233aca285fc19495a57f
Christian <clgreyeyes@email.arizona.edu>
2015-12-19 22:36:01 -0700

    added all files

---
+++ h12/nin.py
@@ -0,0 +1,34 @@
+def write_file( filename ):
+   thefile = open(filename)
+
+   result = []
+
+   i = 1
+
+   for line in thefile:
+      pass1 = line.replace('opulation": "', 'opulation": ').replace('", "LifeE', ', "LifeE')
+      pass1 = pass1.replace('opulation": , "LifeE', 'opulation": -1.0, "LifeE')
+      pass2 = pass1.replace('tancy": "', 'tancy": ').replace('", "GNP"', ', "GNP"')
+      pass2 = pass2.replace('tancy": , "GNP"', 'tancy": -1.0, "GNP"')
+      pass3 = pass2.replace('"GNP": "', '"GNP": ').replace('", "GNPOld"', ', "GNPOld"')
+      pass3 = pass3.replace('"GNP": , "GNPOld"', '"GNP": -1.0, "GNPOld"')
+      pass4 = pass3.replace('"SurfaceArea": "', '"SurfaceArea": ').replace('", "IndepY', ', "IndepY')
+      pass4 = pass4.replace('"SurfaceArea": , "IndepY', '"SurfaceArea": -1.0, "IndepY')
+      print(str(i) + ' ' + pass4)
+      i += 1
+
+      result += pass4
+
+   thefile.close()
+
+   finalr = open('country2.json', 'w')
+
+   for line in result:
+      finalr.write(line)
+
+   finalr.close()
+
+def main():
+   write_file('country1.json')
+
+main()
